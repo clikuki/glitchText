3598f34840428d7c5ddcbb0420ba0c23812f29dc
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-12-14 23:03:05 +0800

    small demo updates

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const stringPool = [
+	'placeholder placeholder placeholder',
+	'Look at my library!',
+	'BOOOOOOO!',
 	'This is a demonstration!',
 	'Here is some text.',
+	'Here is some more text.',
 	'Pretty cool, right?',
 	'Check out my github profile at "www.Github.com/Clikuki"!',
 	'GLIIIIIIITCH!',
 	'I gently open the door.',
-	'I\'m running out of things to write',
+	'I\'m running out of things to write.',
 ]
 
 const glitchDiv = document.querySelector('#glitchedDiv');
 const demoBtn = document.querySelector('#demoBtn');
 const glitchKey = glitchText.register(glitchDiv, {
-	speed: 2,
+	speed: 3,
 });
 
 const getRandInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 demoBtn.addEventListener('click', () =>
 {
-	const randInt = getRandInt(0, stringPool.length - 1);
-	const randStr = stringPool[randInt];
+	const randIndex = getRandInt(0, stringPool.length - 1);
+	const randStr = stringPool[randIndex];
 	glitchText.set(glitchKey, randStr);
 })
